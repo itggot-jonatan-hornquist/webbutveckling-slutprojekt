a9b01d9c6b5a208d16b3a2192e37e2bd6b378159
itggot-jonatan-hornquist <jonatan.hornquist@elev.ga.ntig.se>
2019-05-09 15:21:15 +0200

    Added initial styling and semi-optimal transitioning for article-boxes in content.

--- site/js/main.js
+++ site/js/main.js
@@ -1,8 +1,28 @@
-function activate(event){
-    console.log("wowie this sure works")
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds){
+            break;
+        }
+    }
+}
+
+function toggleMenu(event){
+    console.log("Beep boop bap toggling menu menu")
     let hamMenu = document.querySelector(".menu")
     hamMenu.classList.toggle("menu-activated")
-
+    
     let fade = document.querySelector(".fade")
     fade.classList.toggle("fade-activated")
+}
+
+function toggleFocus(event){
+    console.log("begone element!")
+    let begonner = document.querySelector(".title-card")
+    begonner.classList.toggle("nonexistence")
+    
+    let appearer = document.querySelector(".article-preview")
+    appearer.classList.toggle("fader")
+    appearer.classList.toggle("invisible")
+    
 }
